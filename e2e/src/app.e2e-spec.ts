b3bab5ca9727364cee491baa0211983831d2d4c5
joopar <ryejoon@gmail.com>
2019-08-19 18:34:13 +0900

    rename project

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to split-viewer!');
+    expect(page.getTitleText()).toEqual('Welcome to scroll-pay!');
   });
 
   afterEach(async () => {
